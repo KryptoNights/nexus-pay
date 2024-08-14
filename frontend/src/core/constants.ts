269cfce84b092bbe0ec011d58603c264f927fc23
debjit <debjitbhowal.db@gmail.com>
2024-08-14 12:10:50 +0530

    aptos core init

---
+++ frontend/src/core/constants.ts
@@ -0,0 +1,21 @@
+// Copyright © Aptos
+// SPDX-License-Identifier: Apache-2.0
+
+import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
+
+/**
+ * [FIX ME]
+ * Replace the Google client_id below with your own.
+ * Authorized JavaScript origins: https://pwsfs8-5173.csb.app
+ * Authorized redirect URIs: https://pwsfs8-5173.csb.app/callback
+ */
+export const GOOGLE_CLIENT_ID =
+  "876401151866-eh0r26s88b9r2ef55m4ium8f4k3r2q6j.apps.googleusercontent.com";
+
+export const LocalStorageKeys = {
+  keylessAccounts: "@aptos-connect/keyless-accounts",
+};
+
+export const devnetClient = new Aptos(
+  new AptosConfig({ network: Network.DEVNET }),
+);
