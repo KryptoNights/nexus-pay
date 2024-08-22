5616e18539dfd22fe85f6902f7d6074e58866335
Kavish Shah <kavishshah30@gmail.com>
2024-08-22 21:56:17 +0530

    input box QR  scan made bigger

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -120,8 +120,9 @@ const Home: NextPage = () => {
         </h1>
 
         <div className="w-full max-w-md">
-          <div className="relative">
-            <input
+          <div className="relative w-full">
+            {/* do not remove this commented code */}
+            {/* <input
               type="text"
               placeholder="0x123... or yourname.nexus"
               className="input input-bordered input-primary w-full rounded-full pr-12"
@@ -148,6 +149,34 @@ const Home: NextPage = () => {
                   d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z"
                 />
               </svg>
+            </button> */}
+            <input
+              type="text"
+              placeholder="0x123... or yourname@nexus"
+              className="input input-bordered input-primary w-full pr-24 rounded-full"
+              value={recipientAddress}
+              onChange={handleInputChange}
+            />
+            <button
+              className="absolute right-0 top-0 bottom-0 btn btn-secondary rounded-r-full flex items-center px-4 tooltip tooltip-left"
+              onClick={() => handlePopupOpen()}
+              data-tip="Scan QR Code"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="h-5 w-5 mr-1"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z"
+                />
+              </svg>
+              Scan
             </button>
 
             {suggestions?.length > 0 && (
