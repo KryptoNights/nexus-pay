caa5f07719b2c641c687bf1f0d945f60182059e2
debjit <debjitbhowal.db@gmail.com>
2024-08-26 18:28:37 +0530

    chore: Refactor transactions module and add new functions for sending money to account and ID

--- frontend/src/core/transactions.ts
+++ frontend/src/core/transactions.ts
@@ -6,19 +6,25 @@ import axios from 'axios';
 const aptos = new Aptos(new AptosConfig({ network: Network.TESTNET }));
 
 export const testSendMoneyToAccount = async (address: string, signer: KeylessAccount): Promise<string> => {
-    const transaction = await aptos.transferCoinTransaction({
-        sender: signer.accountAddress,
-        recipient: AccountAddress.fromString(address),
-        amount: 100000000,
-    });
+    if (address.includes("@") || address.includes(".")) {
+        return testSendMoneyToId(address, "", signer);
+    }
+    return sendCoinToAddres(
+        AccountAddress.fromString(address),
+        5000,
+        "0x1::aptos_coin::AptosCoin",
+        signer
+    )
+}
 
-    const committedTxn = await aptos.signAndSubmitTransaction({ signer: signer, transaction });
-
-    const committedTransactionResponse = await aptos.waitForTransaction({ transactionHash: committedTxn.hash });
-    console.log("HASHHHHHH");
-    console.log(committedTransactionResponse);
-
-    return committedTransactionResponse.hash;
+export const testSendMoneyToId = async (id: string, id_token: string, signer: KeylessAccount): Promise<string> => {
+    const wallet = await get_wallet_from_nexus_id(id_token, id);
+    return sendCoinToAddres(
+        AccountAddress.fromString(wallet),
+        5000,
+        "0x1::aptos_coin::AptosCoin",
+        signer
+    )
 }
 
 export const getBalances = async (address: string): Promise<{
@@ -55,7 +61,7 @@ export const getBalances = async (address: string): Promise<{
     }
 }
 
-export const sendCoin = async (recipient: AccountAddress, amount: number, type: string, signer: KeylessAccount): Promise<string> => {
+export const sendCoinToAddres = async (recipient: AccountAddress, amount: number, type: string, signer: KeylessAccount): Promise<string> => {
     const parts = type.split("::");
     if (parts.length !== 3) {
         throw new Error("Invalid coin type, should be in the format of '0x1::aptos_coin::AptosCoin'");
@@ -75,7 +81,15 @@ export const sendCoin = async (recipient: AccountAddress, amount: number, type: 
     return committedTransactionResponse.hash;    
 }
 
-export const get_nexus_ids_starting_with = async (id_token: string, query_string: string): Promise<string[]> => {
+export const get_nexus_ids_starting_with = async (id_token: string, raw_query_string: string): Promise<string[]> => {
+    let query_string = raw_query_string.trim();
+    if (query_string.includes("@")) {
+        query_string = query_string.split("@")[0];
+    }
+    if (query_string.includes(".")) {
+        query_string = query_string.split(".")[0];
+    }
+
     const response = await axios.post(
       'https://nexus-query-startswith-7kxt74l7iq-uc.a.run.app',
       {
@@ -93,3 +107,19 @@ export const get_nexus_ids_starting_with = async (id_token: string, query_string
     return response.data.emails;
   };
   
+  export const get_wallet_from_nexus_id = async (id_token: string, nexus_id: string): Promise<string> => {
+    const response = await axios.post(
+        'https://nexus-fetch-wallet-for-id-7kxt74l7iq-uc.a.run.app',
+        {
+            nexus_id: nexus_id
+        },
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${id_token}`
+            },
+        }
+    );
+    console.log(response);
+    return response.data.wallet;
+  }
