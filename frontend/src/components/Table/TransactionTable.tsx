44e00468bcb73fa52a7418cc1a30e06cc5f2c668
Kavish Shah <kavishshah30@gmail.com>
2024-08-28 20:53:05 +0530

    func updated in transactions

--- frontend/src/components/Table/TransactionTable.tsx
+++ frontend/src/components/Table/TransactionTable.tsx
@@ -3,7 +3,7 @@
 import Layout from "@/components/Layout/Layout";
 import { get_transaction_history } from "@/core/transactions";
 import { useKeylessAccounts } from "@/core/useKeylessAccounts";
-import { divideByTenMillion } from "@/core/utils";
+import { collapseAddress, divideByTenMillion } from "@/core/utils";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
@@ -11,12 +11,12 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 interface Transaction {
-  action: "sent" | "received";
+  version: string;
+  action: string;
   amount: number;
-  gas_fee: number | undefined;
-  sender?: string;
+  gas_fee?: number;
   success: boolean;
-  version: number;
+  sender?: string;
 }
 
 const TransactionTable: NextPage = () => {
@@ -24,8 +24,11 @@ const TransactionTable: NextPage = () => {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
-  const { activeAccountAdress, balance } = useSelector((state: any) => state.authSlice);
+  const { activeAccountAdress, balance } = useSelector(
+    (state: any) => state.authSlice
+  );
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
@@ -34,6 +37,7 @@ const TransactionTable: NextPage = () => {
   useEffect(() => {
     const fetchTransactionHistory = async () => {
       try {
+        setLoading(true);
         const offset = (currentPage - 1) * totalpages;
         const response = await get_transaction_history(
           activeAccountAdress,
@@ -41,6 +45,7 @@ const TransactionTable: NextPage = () => {
         );
         console.log("Transaction history details:", response);
         setTransactions(response as Transaction[]);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching transaction history:", error);
       }
@@ -74,9 +79,12 @@ const TransactionTable: NextPage = () => {
         />
       </div>
 
-      <div className="overflow-x-auto">
+      <div
+        className="overflow-x-auto"
+        style={{ overflowY: "scroll", height: "400px" }}
+      >
         <table className="table w-full">
-          <thead>
+          <thead className="sticky top-0 bg-gray-800 z-10">
             <tr className="bg-gray-800 text-primary">
               <th>Action</th>
               <th>Amount</th>
@@ -85,35 +93,59 @@ const TransactionTable: NextPage = () => {
               <th>Status</th>
             </tr>
           </thead>
-          <tbody>
-            {filteredTransactions.map((transaction, index) => (
-              <tr
-                key={index}
-                className={index % 2 === 0 ? "bg-gray-900" : "bg-gray-800"}
+          {!loading ? (
+            <tbody>
+              {filteredTransactions.map((transaction, index) => (
+                <tr
+                  key={index}
+                  className={index % 2 === 0 ? "bg-gray-900" : "bg-gray-800"}
+                >
+                  <td>{transaction.action}</td>
+                  <td className="text-center">
+                    {divideByTenMillion(transaction?.amount)} APT
+                  </td>
+                  <td className="truncate max-w-[200px]">
+                    {collapseAddress(transaction?.sender || "") || "N/A"}
+                  </td>
+                  <td>
+                    {transaction.gas_fee !== undefined
+                      ? divideByTenMillion(transaction.gas_fee)
+                      : ""}
+                  </td>
+                  <td>
+                    <span
+                      className={`badge ${transaction.success ? "badge-success" : "badge-error"}`}
+                    >
+                      {transaction.success ? "Success" : "Failed"}
+                    </span>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          ) : (
+            <div
+              role="status"
+              className="w-[100vh] flex justify-center items-center"
+            >
+              <svg
+                aria-hidden="true"
+                className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <td>{transaction.action}</td>
-                <td className="text-right">
-                  {divideByTenMillion(transaction?.amount)?.toLocaleString()}{" "}
-                  APT
-                </td>
-                <td className="truncate max-w-[200px]">
-                  {transaction.sender || "N/A"}
-                </td>
-                <td>
-                  {transaction.gas_fee !== undefined
-                    ? divideByTenMillion(transaction.gas_fee)?.toLocaleString()
-                    : "N/A"}
-                </td>
-                <td>
-                  <span
-                    className={`badge ${transaction.success ? "badge-success" : "badge-error"}`}
-                  >
-                    {transaction.success ? "Success" : "Failed"}
-                  </span>
-                </td>
-              </tr>
-            ))}
-          </tbody>
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span className="sr-only">Loading...</span>
+            </div>
+          )}
         </table>
       </div>
 
--- frontend/src/core/transactions.ts
+++ frontend/src/core/transactions.ts
@@ -126,89 +126,205 @@ export const get_wallet_from_nexus_id = async (id_token: string, nexus_id: strin
     return response.data.wallet;
 }
 
-export const get_transaction_history = async (address: string, offset: number): Promise<any> => {
-    const response = await axios.post(
-        'https://api.testnet.aptoslabs.com/v1/graphql',
-        {
-            'query': 'query User_transactions($where: fungible_asset_activities_bool_exp, $orderBy: [fungible_asset_activities_order_by!], $accountTransactionsWhere2: account_transactions_bool_exp, $accountTransactionsOrderBy2: [account_transactions_order_by!], $limit: Int, $fungibleAssetActivitiesLimit2: Int, $offset: Int, $accountTransactionsOffset2: Int) {\n  fungible_asset_activities(where: $where, order_by: $orderBy, limit: $fungibleAssetActivitiesLimit2, offset: $offset) {\n    amount\n    type\n    is_gas_fee\n    is_transaction_success\n    asset_type\n    transaction_version\n  }\n  account_transactions(where: $accountTransactionsWhere2, order_by: $accountTransactionsOrderBy2, limit: $limit, offset: $accountTransactionsOffset2) {\n    transaction_version\n    user_transaction {\n      sender\n    }\n  }\n}',
-            'variables': {
-                'where': {
-                    'owner_address': {
-                        '_eq': address
-                    }
-                },
-                'orderBy': [
-                    {
-                        'transaction_timestamp': 'desc'
-                    }
-                ],
-                'accountTransactionsWhere2': {
-                    'account_address': {
-                        '_eq': address
-                    }
-                },
-                'accountTransactionsOrderBy2': [
-                    {
-                        'transaction_version': 'desc'
-                    }
-                ],
-                'limit': 15,
-                'fungibleAssetActivitiesLimit2': 15,
-                'offset': offset,
-                'accountTransactionsOffset2': 0
-            },
-            'operationName': 'User_transactions'
-        },
-        {
-            headers: {
-                'content-type': 'application/json'
-            }
-        }
-    );
-
-    const raw_fungible_asset_activities = response.data.data.fungible_asset_activities;
-    const raw_account_transactions = response.data.data.account_transactions as any[];
-    const versions = raw_fungible_asset_activities.map((activity: any) => {
-        return activity.transaction_version;
-    });
-    console.log(versions);
-
-    const version_sender_map: { [key: string]: string } = {};
-    for (let transaction of raw_account_transactions) {
-        version_sender_map[transaction.transaction_version as string] = transaction.user_transaction.sender;
-    }
-    // console.log(version_sender_map);
-
-    let version_activity_map: { [key: string]: any } = {};
-    for (let activity of raw_fungible_asset_activities) {
-        // console.log(version_activity_map[activity.transaction_version as string] || {});
-        version_activity_map[activity.transaction_version as string] = version_activity_map[activity.transaction_version as string] || {};
-
-        version_activity_map[activity.transaction_version as string].gas_fee = (activity.is_gas_fee as boolean) ? activity.amount as number : version_activity_map[activity.transaction_version as string].gas_fee;
-        version_activity_map[activity.transaction_version as string].success = activity.is_transaction_success as boolean || false;
-        
-        if ((activity.type as string).endsWith("DepositEvent")) {
-            version_activity_map[activity.transaction_version as string].action = "Received";
-            version_activity_map[activity.transaction_version as string].amount = activity.amount;
-        } else if ((activity.type as string).endsWith("WithdrawEvent")) {
-            version_activity_map[activity.transaction_version as string].action = "Sent";
-            version_activity_map[activity.transaction_version as string].amount = activity.amount;
-        }
-        // console.log(version_activity_map[activity.transaction_version as string]);
-        // console.log("\n\n\n");
-        version_activity_map[activity.transaction_version as string].sender = version_sender_map[activity.transaction_version as string];
-    }
-
-    const history = [];
-    for (let version of versions) {
-        if (version_activity_map[version]) {
-            history.push({
-                version,
-                ...version_activity_map[version],
-            });
-        }
-    }
-
-    console.log(history);
-    return history;
+interface FungibleAssetActivity {
+    amount: number;
+    type: string;
+    is_gas_fee: boolean;
+    is_transaction_success: boolean;
+    asset_type: string;
+    transaction_version: string;
 }
+
+interface AccountTransaction {
+    transaction_version: string;
+    user_transaction: {
+        sender: string;
+    };
+}
+
+interface TransactionHistory {
+    version: string;
+    action: string;
+    amount: number;
+    gas_fee?: number;
+    success: boolean;
+    sender?: string;
+}
+
+export const get_transaction_history = async (address: string, offset: number): Promise<TransactionHistory[]> => {
+    try {
+        const response = await axios.post(
+            'https://api.testnet.aptoslabs.com/v1/graphql',
+            {
+                query: `
+            query User_transactions($where: fungible_asset_activities_bool_exp, $orderBy: [fungible_asset_activities_order_by!], $accountTransactionsWhere2: account_transactions_bool_exp, $accountTransactionsOrderBy2: [account_transactions_order_by!], $limit: Int, $fungibleAssetActivitiesLimit2: Int, $offset: Int, $accountTransactionsOffset2: Int) {
+              fungible_asset_activities(where: $where, order_by: $orderBy, limit: $fungibleAssetActivitiesLimit2, offset: $offset) {
+                amount
+                type
+                is_gas_fee
+                is_transaction_success
+                asset_type
+                transaction_version
+              }
+              account_transactions(where: $accountTransactionsWhere2, order_by: $accountTransactionsOrderBy2, limit: $limit, offset: $accountTransactionsOffset2) {
+                transaction_version
+                user_transaction {
+                  sender
+                }
+              }
+            }
+          `,
+                variables: {
+                    where: {
+                        owner_address: { _eq: address }
+                    },
+                    orderBy: [{ transaction_timestamp: 'desc' }],
+                    accountTransactionsWhere2: {
+                        account_address: { _eq: address }
+                    },
+                    accountTransactionsOrderBy2: [{ transaction_version: 'desc' }],
+                    limit: 15,
+                    fungibleAssetActivitiesLimit2: 15,
+                    offset: offset,
+                    accountTransactionsOffset2: 0
+                },
+                operationName: 'User_transactions'
+            },
+            {
+                headers: { 'content-type': 'application/json' }
+            }
+        );
+
+        const raw_fungible_asset_activities: FungibleAssetActivity[] = response.data.data.fungible_asset_activities;
+        console.log("raw", raw_fungible_asset_activities);
+        
+        const raw_account_transactions: AccountTransaction[] = response.data.data.account_transactions;
+
+        const version_sender_map = new Map<string, string>();
+        for (const transaction of raw_account_transactions) {
+            version_sender_map.set(transaction.transaction_version, transaction.user_transaction.sender);
+        }
+
+        const version_activity_map = new Map<string, TransactionHistory>();
+        for (const activity of raw_fungible_asset_activities) {
+            const existingActivity: any = version_activity_map.get(activity.transaction_version) || {
+                version: activity.transaction_version,
+                success: false,
+                amount: 0,
+            };
+
+            if (activity.is_gas_fee) {
+                existingActivity.gas_fee = activity.amount;
+            }
+
+            existingActivity.success = existingActivity.success || activity.is_transaction_success;
+
+            if (activity.type.endsWith("DepositEvent")) {
+                existingActivity.action = "Received";
+                existingActivity.amount = activity.amount;
+            } else if (activity.type.endsWith("WithdrawEvent")) {
+                existingActivity.action = "Sent";
+                existingActivity.amount = activity.amount;
+            }
+
+            existingActivity.sender = version_sender_map.get(activity.transaction_version);
+
+            version_activity_map.set(activity.transaction_version, existingActivity);
+        }
+
+        const history: TransactionHistory[] = Array.from(version_activity_map.values());
+        history.sort((a, b) => parseInt(b.version) - parseInt(a.version));
+
+        return history;
+    } catch (error) {
+        console.error('Error fetching transaction history:', error);
+        throw new Error('Failed to fetch transaction history');
+    }
+};
+
+// export const get_transaction_history = async (address: string, offset: number): Promise<any> => {
+//     const response = await axios.post(
+//         'https://api.testnet.aptoslabs.com/v1/graphql',
+//         {
+//             'query': 'query User_transactions($where: fungible_asset_activities_bool_exp, $orderBy: [fungible_asset_activities_order_by!], $accountTransactionsWhere2: account_transactions_bool_exp, $accountTransactionsOrderBy2: [account_transactions_order_by!], $limit: Int, $fungibleAssetActivitiesLimit2: Int, $offset: Int, $accountTransactionsOffset2: Int) {\n  fungible_asset_activities(where: $where, order_by: $orderBy, limit: $fungibleAssetActivitiesLimit2, offset: $offset) {\n    amount\n    type\n    is_gas_fee\n    is_transaction_success\n    asset_type\n    transaction_version\n  }\n  account_transactions(where: $accountTransactionsWhere2, order_by: $accountTransactionsOrderBy2, limit: $limit, offset: $accountTransactionsOffset2) {\n    transaction_version\n    user_transaction {\n      sender\n    }\n  }\n}',
+//             'variables': {
+//                 'where': {
+//                     'owner_address': {
+//                         '_eq': address
+//                     }
+//                 },
+//                 'orderBy': [
+//                     {
+//                         'transaction_timestamp': 'desc'
+//                     }
+//                 ],
+//                 'accountTransactionsWhere2': {
+//                     'account_address': {
+//                         '_eq': address
+//                     }
+//                 },
+//                 'accountTransactionsOrderBy2': [
+//                     {
+//                         'transaction_version': 'desc'
+//                     }
+//                 ],
+//                 'limit': 15,
+//                 'fungibleAssetActivitiesLimit2': 15,
+//                 'offset': offset,
+//                 'accountTransactionsOffset2': 0
+//             },
+//             'operationName': 'User_transactions'
+//         },
+//         {
+//             headers: {
+//                 'content-type': 'application/json'
+//             }
+//         }
+//     );
+
+//     const raw_fungible_asset_activities = response.data.data.fungible_asset_activities;
+//     const raw_account_transactions = response.data.data.account_transactions as any[];
+//     const versions = raw_fungible_asset_activities.map((activity: any) => {
+//         return activity.transaction_version;
+//     });
+//     console.log(versions);
+
+//     const version_sender_map: { [key: string]: string } = {};
+//     for (let transaction of raw_account_transactions) {
+//         version_sender_map[transaction.transaction_version as string] = transaction.user_transaction.sender;
+//     }
+//     // console.log(version_sender_map);
+
+//     let version_activity_map: { [key: string]: any } = {};
+//     for (let activity of raw_fungible_asset_activities) {
+//         // console.log(version_activity_map[activity.transaction_version as string] || {});
+//         version_activity_map[activity.transaction_version as string] = version_activity_map[activity.transaction_version as string] || {};
+
+//         version_activity_map[activity.transaction_version as string].gas_fee = (activity.is_gas_fee as boolean) ? activity.amount as number : version_activity_map[activity.transaction_version as string].gas_fee;
+//         version_activity_map[activity.transaction_version as string].success = activity.is_transaction_success as boolean || false;
+        
+//         if ((activity.type as string).endsWith("DepositEvent")) {
+//             version_activity_map[activity.transaction_version as string].action = "Received";
+//             version_activity_map[activity.transaction_version as string].amount = activity.amount;
+//         } else if ((activity.type as string).endsWith("WithdrawEvent")) {
+//             version_activity_map[activity.transaction_version as string].action = "Sent";
+//             version_activity_map[activity.transaction_version as string].amount = activity.amount;
+//         }
+//         // console.log(version_activity_map[activity.transaction_version as string]);
+//         // console.log("\n\n\n");
+//         version_activity_map[activity.transaction_version as string].sender = version_sender_map[activity.transaction_version as string];
+//     }
+
+//     const history = [];
+//     for (let version of versions) {
+//         if (version_activity_map[version]) {
+//             history.push({
+//                 version,
+//                 ...version_activity_map[version],
+//             });
+//         }
+//     }
+
+//     console.log(history);
+//     return history;
+// }
