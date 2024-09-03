eb9cd17f3c478cfd2173b94ffb11e09f8c2e733f
Akki <codewithakki@gmail.com>
2024-09-03 18:07:58 +0530

    events

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -6,7 +6,13 @@ import store from "../redux/store";
 import { Provider, useSelector } from "react-redux";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
+import mixpanel from "mixpanel-browser";
 
+mixpanel.init("90e1bef61bd9e8539fe7fed160938e58", {
+  debug: true,
+  track_pageview: true,
+  persistence: "localStorage",
+});
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   // suppress useLayoutEffect warnings when running outside a browser
