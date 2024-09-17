acc6ce74fd7d086aeb8eb77c7612ea844cc8134a
Kavish Shah <kavishshah30@gmail.com>
2024-09-17 22:31:15 +0530

    fix

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -45,6 +45,7 @@ function LoginPage() {
 
   const router = useRouter();
   const openPopup = () => {
+    mixpanel.track("login_inititated");
     const popup = window.open(redirectUrl, "popup", "popup=true");
     if (!popup) {
       alert("Popup blocked! Please allow popups for this site.");
