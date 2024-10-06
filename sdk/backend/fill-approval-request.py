48bfd79da21a4b295bec41e4ade7611b32bd226f
debjit <debjitbhowal.db@gmail.com>
2024-10-07 00:39:38 +0530

    basic backend for sdk

---
+++ sdk/backend/fill-approval-request.py
@@ -0,0 +1,54 @@
+import functions_framework
+from flask_cors import cross_origin
+from firebase_admin import firestore, initialize_app
+import google.cloud.firestore
+import requests as req
+import random
+
+initialize_app()
+db: google.cloud.firestore.Client = firestore.client()
+
+@functions_framework.http
+@cross_origin()
+def hello_http(request):
+    request_auth_token = request.headers.get('Authorization').split(' ')[1]
+    if not request_auth_token:
+        return {"error": "Provide authorization token"}
+    
+    request_id = None
+    tx_hash = None
+    email = None
+
+    if request.content_type == 'application/x-www-form-urlencoded':
+        request_id = request.form.get('id')
+        tx_hash = request.form.get('tx_hash')
+        email = request.form.get('email')
+    elif request.content_type == 'application/json':
+        request_id = request.get_json().get('id')
+        tx_hash = request.get_json().get('tx_hash')
+        email = request.get_json().get('email')
+
+    if not request_id or not tx_hash or not email:
+        return {"error": "Provide all details: id, tx_hash, email"}
+    
+    doc_ref = db.collection("nexuspay").document("email_mapping").collection("emails").document(email)
+    doc = doc_ref.get().to_dict()
+
+    if not doc:
+        return {"error": "Email not found"}
+    
+    approvals = doc.get('approvals')
+    if not approvals:
+        approvals = []
+
+    # mark approval request as approved and add tx_hash
+    for approval in approvals:
+        if approval['id'] == request_id:
+            approval['is_filled'] = True
+            approval['tx_hash'] = tx_hash
+            break
+
+    doc_ref.set({'approvals': approvals}, merge=True)
+
+    return {"message": "Approval request filled"}
+    
---
+++ sdk/backend/send-approval-request.py
@@ -0,0 +1,68 @@
+import functions_framework
+from flask_cors import cross_origin
+from firebase_admin import firestore, initialize_app
+import google.cloud.firestore
+import requests as req
+import random
+
+def random_id(n = 12):
+    return ''.join(random.choices('0123456789QWERTYUIOPASDFGHJKLZXCVBNMwertyuiopsdfghjklzxcvbnm', k=n))
+
+initialize_app()
+db: google.cloud.firestore.Client = firestore.client()
+
+@functions_framework.http
+@cross_origin()
+def hello_http(request):
+    request_auth_token = request.headers.get('Authorization').split(' ')[1]
+    if not request_auth_token:
+        return {"error": "Provide authorization token"}
+
+    # TODO: Implement verify function and change architecture to prevent races
+    # ...
+
+    request_name = None
+    request_details = None
+    request_amount = None
+    request_token = None
+    email_to_request = None
+
+    if request.content_type == 'application/x-www-form-urlencoded':
+        request_name = request.form.get('name')
+        request_details = request.form.get('details')
+        request_amount = request.form.get('amount')
+        request_token = request.form.get('token')
+        email_to_request = request.form.get('email_to_request')
+    elif request.content_type == 'application/json':
+        request_name = request.get_json().get('name')
+        request_details = request.get_json().get('details')
+        request_amount = request.get_json().get('amount')
+        request_token = request.get_json().get('token')
+        email_to_request = request.get_json().get('email_to_request')
+
+    if not request_name or not request_details or not request_amount or not request_token or not email_to_request:
+        return {"error": "Provide all details: name, details, amount, token"}
+    
+    doc_ref = db.collection("nexuspay").document("email_mapping").collection("emails").document(email_to_request)
+    doc = doc_ref.get().to_dict()
+
+    if not doc:
+        return {"error": "Email not found"}
+
+    approvals = doc.get('approvals')
+    if not approvals:
+        approvals = []
+
+    # add approval request to start of list
+    approvals.insert(0, {
+        "name": request_name,
+        "details": request_details,
+        "amount": request_amount,
+        "token": request_token,
+        "id": random_id(),
+        "is_filled": False
+    })
+
+    doc_ref.set({"approvals": approvals}, merge=True)
+
+    return {"message": None, "added": True, "approvals": approvals}
