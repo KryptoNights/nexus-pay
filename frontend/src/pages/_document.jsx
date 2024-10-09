bdcca2b133265f9e16950f63d426f794809ab63e
Kavish Shah <kavishshah30@gmail.com>
2024-10-09 23:10:29 +0530

    added meta tags and google console verification

--- frontend/src/pages/_document.jsx
+++ frontend/src/pages/_document.jsx
@@ -1,13 +1,25 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html className="antialiased [font-feature-settings:'ss01']" lang="en">
-      <Head>{}</Head>
+      <Head>
+        <meta
+          name="keywords"
+          content="Nexus, NexusPay, web3, Web3, Crypto, payments, payments sdk, easy payments, Eth Global"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="google-site-verification"
+          content="NqX5mc07r18m3it3uZ5VvRW_mkz7NmFQ5r6ZZM8IRGk"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
