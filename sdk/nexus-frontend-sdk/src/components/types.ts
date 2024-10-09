cfd5122eb7835d843b58cce5297c540ba4942c22
Akki <codewithakki@gmail.com>
2024-10-09 18:06:14 +0530

    0.010

--- sdk/nexus-frontend-sdk/src/components/types.ts
+++ sdk/nexus-frontend-sdk/src/components/types.ts
@@ -2,7 +2,6 @@ export interface RequestIframeProps {
     name: string;
     details: string;
     amount: string;
-    buttonClassName?: string;
     onClose: () => void;
     open: Boolean;
     onClick: () => void;
